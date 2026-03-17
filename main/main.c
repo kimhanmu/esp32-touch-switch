4c5277c75a84921199a84e69b1a8f4fa0df0a981
hanmu kim <hanmukim@hanmuui-MacBookPro.local>
2026-03-17 13:23:01 +0900

    Initial commit: 7-inch LCD UI and Touch Success

---
+++ main/main.c
@@ -0,0 +1,29 @@
+/*
+ * SPDX-FileCopyrightText: 2023-2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include "ui/ui.h"
+#include "waveshare_rgb_lcd_port.h"
+
+void app_main()
+{
+    waveshare_esp32_s3_rgb_lcd_init(); // Initialize the Waveshare ESP32-S3 RGB LCD 
+    // wavesahre_rgb_lcd_bl_on();  //Turn on the screen backlight 
+    // wavesahre_rgb_lcd_bl_off(); //Turn off the screen backlight 
+    
+    ESP_LOGI(TAG, "Display LVGL demos");
+    // Lock the mutex due to the LVGL APIs are not thread-safe
+    if (lvgl_port_lock(-1)) {
+        // lv_demo_stress();
+        // lv_demo_benchmark();
+        // lv_demo_music();
+        // lv_demo_widgets();
+        // example_lvgl_demo_ui();
+        // 드디어 도화지에 버튼을 그립니다!
+           ui_init();
+        // Release the mutex
+        lvgl_port_unlock();
+    }
+}
